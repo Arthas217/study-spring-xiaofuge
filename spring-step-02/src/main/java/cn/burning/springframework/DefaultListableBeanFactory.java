b46462ebcd1c3846bcb4029a799b9c64f6dfb9b0
zuocheng <442854399@qq.com>
2022-11-18 10:31:23 +0800

    spring-step-02

--- spring-step-02/src/main/java/cn/burning/springframework/DefaultListableBeanFactory.java
+++ spring-step-02/src/main/java/cn/burning/springframework/DefaultListableBeanFactory.java
@@ -11,17 +11,21 @@ import java.util.Map;
 /**
  * @Author 会游泳的蚂蚁
  * @Description: 默认的Bean工厂实现类（核心实现类）
+ * 此类继承了AbstractAutowireCapableBeanFactory 类，也就具备了接口 BeanFactory 和 AbstractBeanFactory 等一连串的功能实现。
+ * 所以有时候你会看到一些类的强转，调用某些方法（例如此类中createBean方法），也是因为你强转的类实现接口或继承了某些类。
+ *
+ * 此类中涉及的BeanDefinitionRegistry接口定义了注册，AbstractBeanFactory抽象类定义了获取，都集中在beanDefinitionMap里
  * @Date 2022/11/17 22:41
  */
 public class DefaultListableBeanFactory extends AbstractAutowireCapableBeanFactory implements BeanDefinitionRegistry {
 
     /**
-     * spring bean容器
+     * spring bean容器  且依赖BeanDefinition
      */
     private Map<String, BeanDefinition> beanDefinitionMap = new HashMap<>();
 
     /**
-     * 注册
+     * 注册bean定义
      * @param beanName       Bean 名称
      * @param beanDefinition Bean 定义
      */
@@ -31,7 +35,8 @@ public class DefaultListableBeanFactory extends AbstractAutowireCapableBeanFacto
     }
 
     /**
-     * 只需要关心属于自己相关内容，获取bean定义---实现了抽象方法1
+     * 获取bean定义
+     * 只需要关心属于自己相关内容，获取bean定义（基类的基类）---实现了抽象方法1
      * @param beanName
      * @return
      */
