b271cd21b91a77726a2ff8defd3e9eb1f7ccd964
zuocheng <442854399@qq.com>
2023-03-31 00:23:57 +0800

    spring-step-06

--- spring-step-06/src/main/java/cn/burning/springframework/context/ConfigurableApplicationContext.java
+++ spring-step-06/src/main/java/cn/burning/springframework/context/ConfigurableApplicationContext.java
@@ -13,7 +13,6 @@ public interface ConfigurableApplicationContext extends ApplicationContext {
 
     /**
      * 刷新容器
-     *
      * @throws BeansException
      */
     void refresh() throws BeansException;
